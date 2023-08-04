7283b5f32a780f30adf627971d0e8882466f3210
Matias Arno <amatiasarno@gmail.com>
2023-08-04 00:20:40 -0300

    Ejercicios terminados

--- src/ejercicio-2.ts
+++ src/ejercicio-2.ts
@@ -1,28 +1,68 @@
-class Individual {
-    name;
-    lastname;
+// Para pasar el test, completar dónde esté incompleto, y corregir los tipos unknown.
 
-    constructor(name: string, lastname: string) {
-        this.name = name;
-        this.lastname = lastname;
-    }
+class Individual {
+	name;
+	age;
+
+	constructor(name: unknown, age: unknown) {
+		// Completar
+	}
 }
 
 class Family {
-    members;
-    name;
+	members;
+	name;
 
-    constructor(members: Individual[], name: string) {
-        this.members = members;
-        this.name = name;
-    }
+	constructor(members: unknown, name: unknown) {
+		// Completar
+	}
+
+	getOldestMember() {
+		const membersAges = this.members.map((individual) => individual.age);
+		const maxAge = // Acá debería de estar el número más grande del array membersAges.
+		return maxAge;
+	}
 }
 
 class Society {
-    members;
+	members;
 
-    constructor(members: Family[]) {
-        this.members = members;
-    }
+	constructor(members: unknown) {
+		// Completar
+	}
+
+	getMembers() {
+		// Completar
+	}
 }
 
+// NO MODIFICAR -------------
+
+(function testFamily() {
+	const a = new Individual('A', 26);
+	const b = new Individual('B', 19);
+	const c = new Individual('C', 58);
+
+	const one = new Individual('1', 36);
+	const two = new Individual('2', 28);
+	const three = new Individual('3', 2);
+
+	const lettersFamily = new Family([a, b, c], 'Letters Clan');
+	const numbersFamily = new Family([one, two, three], 'Numbers Clan');
+
+	const society = new Society([lettersFamily, numbersFamily]);
+
+	const oldestLetterMember = lettersFamily.getOldestMember();
+	const oldestNumberMember = numbersFamily.getOldestMember();
+
+	const societyMembers = society.getMembers().length;
+
+	if (
+		oldestLetterMember === 58 &&
+		oldestNumberMember === 36 &&
+		societyMembers === 2
+	)
+		return console.log('\x1b[32m All tests passed!!! \x1b[0m');
+
+	throw new Error('Tests not passed...');
+})();
